18c4a1f25ffa4a093c8d2039fb0e7e850fe7b939
Felipe Marcon <felipeemarcon@gmail.com>
2020-08-04 18:03:42 -0300

    feat: styles to landing page - mobile and desktop

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+
+// Images
 import logoImage from "../../assets/images/logo.svg";
 import landingImage from "../../assets/images/landing.svg";
-
 import studyIcon from "../../assets/images/icons/study.svg";
 import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
 import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
+
+// Styles
+import "./styles.css";
 
 function Landing() {
   return (
@@ -20,13 +24,13 @@ function Landing() {
           className="hero-image"
         />
         <div className="buttons-container">
-          <a href="" className="study">
+          <a href="#" className="study">
             <img src={studyIcon} alt="Estudar" />
             Estudar
           </a>
-          <a href="" className="give-classes">
-            <img src={giveClassesIcon} alt="Dar aula" />
-            Estudar
+          <a href="#" className="give-classes">
+            <img src={giveClassesIcon} alt="Dar aulas" />
+            Dar aulas
           </a>
         </div>
 
